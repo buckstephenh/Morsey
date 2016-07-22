9390c15a0e83cc51f04999531120f93f73394209
Stephen Buck <stephenherronbuck@gmail.com>
2016-07-22 15:57:11 -0400

    Initial Commit

---
+++ Morsey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Morsey
+//
+//  Created by Stephen Buck on 7/22/16.
+//  Copyright © 2016 Stephen Buck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
